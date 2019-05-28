25e0ca598f12aba026e7849a39a9b6d2e556c188
Matthew Peoples <matthew.peoples@yale.edu>
2019-05-27 22:47:27 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+    def name
+        @name
+    end
+
+    def name=(name_)
+        @name = name_
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(breed_)
+        @breed = breed_
+    end
+end
+
+# fido = Dog.new
+# fido.name("Fido")
