7337d82a58c494f9cd837e60e1e01afa1abc195d
maxsunnyday <max.sun@yale.edu>
2019-05-27 23:44:03 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def name=(their_name)
+        @name = their_name
+    end
+    
+    def name
+        @name
+    end
+
+    def breed=(their_breed)
+        @breed = their_breed
+    end
+
+    def breed
+        @breed
+    end
+end
